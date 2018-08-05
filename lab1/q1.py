1ad5d4b6a0a0adfa982229328df92178583006fb
Vismay Raj <vismayraj1@gmail.com>
2018-08-05 15:32:17 +0530

    new

---
+++ lab1/q1.py
@@ -0,0 +1,8 @@
+#========================================
+#created by: Vismay Raj
+#language  : Python
+#========================================
+
+class agent:
+	def __init__(self):
+		self.agent_loc=agent_loc
